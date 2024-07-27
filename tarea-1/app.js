477c95607a71b2f440703aadee36feac181c328f
suarez <willian.suarezramos@gmail.com>
2024-07-27 12:21:22 -0500

    ejercicos DOM reac

---
+++ tarea-1/app.js
@@ -0,0 +1,9 @@
+let nuevoDiv = document.createElement("div")
+nuevoDiv.id = "miDiv";
+
+let texto = document.createTextNode("Este es un texto dentro del div.");
+nuevoDiv.appendChild(texto);
+
+document.body.appendChild(nuevoDiv);
+
+document.getElementById("contenedor").appendChild(nuevoDiv);
