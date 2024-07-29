0f4702a0691e037bb063b601f0a06b30af2ca82c
suarez <willian.suarezramos@gmail.com>
2024-07-29 13:47:56 -0500

    se realiza tarea 2 del proyect

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>JAMES RODRIGHEZ</h1>
+      <img src="" alt="Foto personaje" />
+      <p>
+        Uno de los grandes jugadores enla historia colombiana, con 33 años todavia tiene madera 
+        para jugar con su ecselente surda y darnos alegrias a los colombianos
+      </p>
+    </div>
+  );
+}
+
+
+export default App;
